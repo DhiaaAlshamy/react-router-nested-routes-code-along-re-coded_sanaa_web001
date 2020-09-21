fa9769729f1b266d1c9b3eebc6056a3f2b898f61
Dhiaa Alshamy <dhodhof@gmail.com>
2020-09-21 20:54:49 +0000

    Automatically backed up by Learn

--- src/containers/MoviesPage.js
+++ src/containers/MoviesPage.js
@@ -1,12 +1,14 @@
-// .src/containers/MoviesPage.js
 import React from 'react';
 import { Route } from 'react-router-dom';
 import MoviesList from '../components/MoviesList';
-
+import MovieShow from '../components/MovieShow';
+ 
 const MoviesPage = ({ match, movies }) => (
   <div>
     <MoviesList movies={movies} />
+    // Adding code to pass the movies to the `MovieShow` component
+    <Route path={`${match.url}/:movieId`} component={<MovieShow movies={movies} /> }/>
   </div>
 )
-
+ 
 export default MoviesPage
